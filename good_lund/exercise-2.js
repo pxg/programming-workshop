7091104a2edd6910d433b9d17ef1e422fb297a0d
Pete Goodman <thegingerbloke@users.noreply.github.com>
2016-01-29 15:59:10 +0000

    Add Good Lund workshop solutions

---
+++ good_lund/exercise-2.js
@@ -0,0 +1,31 @@
+var fs = require("fs");
+var csv = require("csv");
+
+// Get timestamp & filename from arguments
+var givenTimestamp = process.argv[2];
+var filename = process.argv[3];
+
+// Open file and run checking code when loaded
+fs.readFile(filename, "utf8", parseData);
+
+function parseData (err, data) {
+    csv.parse(data, checkShows);
+}
+
+function checkShows (err, tvShows) {
+    var show = tvShows.reduce(compareTimes);
+
+    // output current show variable
+    console.log(show.name, show.seekTime);
+}
+
+function compareTimes (previousValue, currentValue) {
+    if (currentValue[0] < givenTimestamp) {
+        return {
+            'name': currentValue[1],
+            'seekTime': Math.abs(currentValue[0] - givenTimestamp)
+        };
+    } else {
+        return previousValue;
+    }
+}
